4f581fd9180b6db620a488e513e940aba1b4e881
Nishatmoni <shamsuzzahan2280@gmail.com>
2021-11-02 22:13:25 +0600

    base url set

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,2 +1,2 @@
 //export const baseUrl = "http://localhost:5000";
-export const baseUrl = "https://spooky-shadow-06939.herokuapp.com";
+export const baseUrl = "https://salty-falls-25899.herokuapp.com";
